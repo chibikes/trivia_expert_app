abc5d8a8c8160f2ba4f0ac125a1c7c61e15f02fc
OKOH <okoh_chibuike@yahoo.com>
2022-08-06 20:44:20 +0100

    add gamedetails field

--- lib/user_bloc/cubit/user_state.dart
+++ lib/user_bloc/cubit/user_state.dart
@@ -6,9 +6,10 @@ class UserState extends Equatable {
   final User? user;
   final int highScore;
   final int xp;
+  final UserGameDetails gameDetails;
   final List<GameState> gameStates;
 
-  const UserState({this.xp = 0, this.highScore = 0,this.gameStates = const [], this.user = User.empty, this.homeStatus = HomeStatus.waiting}); /// default state of item is zero which is tab zero
+  const UserState({this.xp = 0, this.highScore = 0,this.gameStates = const [], this.user = User.empty, this.homeStatus = HomeStatus.waiting, this.gameDetails = const UserGameDetails()}); /// default state of item is zero which is tab zero
 
   UserState copyWith({
     HomeStatus? homeStatus,
@@ -16,6 +17,7 @@ class UserState extends Equatable {
     int? highScore,
     int? xp,
     List<GameState>? gameStates,
+    UserGameDetails? gameDetails,
 
 }) {
     return UserState(
@@ -24,9 +26,10 @@ class UserState extends Equatable {
       xp: xp ?? this.xp,
       gameStates: gameStates ?? this.gameStates,
       highScore: highScore ?? this.highScore,
+      gameDetails: gameDetails ?? this.gameDetails,
     );
 }
   @override
-  List<Object> get props => [xp, highScore, homeStatus, user!, gameStates];
+  List<Object> get props => [xp, highScore, homeStatus, user!, gameStates, gameDetails];
 
 }
