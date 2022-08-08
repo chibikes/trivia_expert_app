b6489e3a8f20edebc1be1da260405500fbe59b2e
OKOH <okoh_chibuike@yahoo.com>
2022-08-08 11:44:36 +0100

    refactor method: updateScore - add gameDetails parameter to updateScore method

--- lib/high_score_repo/high_score_repo.dart
+++ lib/high_score_repo/high_score_repo.dart
@@ -1,24 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-
 import '../main_models/user_game_details.dart';
 
 class GameRepository {
   static final highScores = FirebaseFirestore.instance.collection('highScores');
   static Future<void> updateScore(
-    String id,
-    int highScore,
-    String photoUrl,
-    String userName,
+      String id,
+      UserGameDetails gameDetails,
   ) {
-      return highScores.doc(id).update({
-        'highScore': highScore,
-        'avatarUrl': photoUrl,
-      }).onError((error, stackTrace) => highScores.doc(id).set({
-        'highScore': highScore,
-        'avatarUrl': photoUrl,
-        'id': id,
-        'userName': userName,
-      }));
+      return highScores.doc(id).update(
+        gameDetails.toMap()
+      ).onError((error, stackTrace) => highScores.doc(id).set(gameDetails.toMap()));
   }
   Stream<UserGameDetails> getUserGameDetails(String id) {
     return highScores.doc(id).snapshots().map((event) => UserGameDetails.fromMap(event.data()! as Map<String, int>));
