063634cf3e83dcefb037398620f6287bb2365674
chibikes <okoh_chibuike@yahoo.com>
2023-05-23 02:13:54 +0100

    format document

--- lib/widgets/power_up_container.dart
+++ lib/widgets/power_up_container.dart
@@ -6,36 +6,64 @@ class PowerUpContainer extends StatelessWidget {
   final double width;
   final String powerUpQty;
   final Widget powerUpIcon;
+  final double fontSize;
   final radius = 5.0;
 
-  const PowerUpContainer({Key? key, this.height = 0, this.width = 0, this.powerUpQty = '', required this.powerUpIcon}) : super(key: key);
+  const PowerUpContainer(
+      {Key? key,
+      this.height = 0,
+      this.width = 0,
+      this.fontSize = 14,
+      this.powerUpQty = '',
+      required this.powerUpIcon})
+      : super(key: key);
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Row(
       children: [
         Container(
           height: height,
-          width: 0.80 * width,
+          width: 0.30 * width,
           decoration: BoxDecoration(
-            color: Colors.blue,
-            borderRadius: BorderRadius.only(topLeft: Radius.circular(radius), bottomLeft: Radius.circular(radius)),
-            border: const Border(),
-          ),
-          child: Center(child: Text(powerUpQty,style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),)),
-        ),
-        Container(
-          height: height,
-          width: 0.20 * width,
-          decoration: BoxDecoration(
-            color: Color(0xff23395d),
-            borderRadius: BorderRadius.only(topRight: Radius.circular(radius), bottomRight: Radius.circular(radius)),
+            color: Colors.black54,
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(radius),
+                bottomLeft: Radius.circular(radius)),
             border: const Border(),
           ),
           child: powerUpIcon,
+          // Center(
+          //     child: Text(
+          //   powerUpQty,
+          //   style: TextStyle(
+          //       fontSize: fontSize,
+          //       color: Colors.white,
+          //       fontWeight: FontWeight.bold),
+          // ),
+          // ),
+        ),
+        Container(
+          height: height,
+          width: 0.70 * width,
+          decoration: BoxDecoration(
+            color: Colors.teal,
+            borderRadius: BorderRadius.only(
+                topRight: Radius.circular(radius),
+                bottomRight: Radius.circular(radius)),
+            border: const Border(),
+          ),
+          child: //
+              Center(
+            child: Text(
+              powerUpQty,
+              style: TextStyle(
+                  fontSize: fontSize,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
         ),
       ],
     );
   }
-
 }
