7c85299793c4216013ff47333768246a1da7382a
chibikes <okoh_chibuike@yahoo.com>
2023-07-03 20:31:16 +0100

    Format document

--- lib/high_score_repo/high_score_repo.dart
+++ lib/high_score_repo/high_score_repo.dart
@@ -3,24 +3,36 @@ import '../main_models/user_game_details.dart';
 
 class GameRepository {
   static final highScores = FirebaseFirestore.instance.collection('highScores');
-  static Future<void> updateScore( String id, UserGameDetails gameDetails) {
-      return highScores.doc(id).update(
-        gameDetails.toMap()
-      ).onError((error, stackTrace) => highScores.doc(id).set(gameDetails.toMap()));
+  static Future<void> updateScore(String id, UserGameDetails gameDetails) {
+    return highScores.doc(id).update(gameDetails.toMap()).onError(
+        (error, stackTrace) => highScores.doc(id).set(gameDetails.toMap()));
   }
+
   Stream<UserGameDetails> getUserGameDetails(String id) {
     try {
-      return highScores.doc(id).snapshots().map((event) => UserGameDetails.fromMap(event.data()!));
-    }catch(e) {
+      return highScores
+          .doc(id)
+          .snapshots()
+          .map((event) => UserGameDetails.fromMap(event.data()!));
+    } catch (e) {
       throw 'exception is: $e';
     }
   }
+
   Stream<List<Map<String, dynamic>>> getLeaderBoard() {
-    return highScores.orderBy('highScore',descending: true).snapshots().map((event)  {
+    var utc = DateTime.now().toUtc();
+    utc.copyWith(hour: 0, second: 0, minute: 0, millisecond: 0, microsecond: 0);
+    return highScores
+        .orderBy('highScore', descending: true)
+        // .where("time" isEqualTo: )
+        .snapshots()
+        .map((event) {
       return event.docs.map((e) => e.data()).toList();
     });
   }
+
   Future<void> updateUserGameDetails(String id, UserGameDetails gameDetails) {
-    return highScores.doc(id).update(gameDetails.toMap()).onError((error, stackTrace) => highScores.doc(id).set(gameDetails.toMap()));
+    return highScores.doc(id).update(gameDetails.toMap()).onError(
+        (error, stackTrace) => highScores.doc(id).set(gameDetails.toMap()));
   }
 }
