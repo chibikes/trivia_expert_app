cf02fa3db045c4eacab9a1bc13b805465d0e3356
chibikes <okoh_chibuike@yahoo.com>
2023-07-03 20:18:18 +0100

    Add new field `time`

--- lib/learderboard_bloc/bloc/leaderboard_state.dart
+++ lib/learderboard_bloc/bloc/leaderboard_state.dart
@@ -1,29 +1,34 @@
 import 'package:equatable/equatable.dart';
 
-enum LeaderBoardStatus {fetching, fetched, failed}
+enum LeaderBoardStatus { fetching, fetched, failed }
+
 class LeaderBoardState extends Equatable {
   final List<Map<String, dynamic>> gameScores;
   final int position;
   final LeaderBoardStatus leaderBoardStatus;
+  final String time;
 
   LeaderBoardState({
     this.leaderBoardStatus = LeaderBoardStatus.fetching,
     this.gameScores = const [],
     this.position = 0,
+    this.time = '',
   });
 
   LeaderBoardState copyWith({
     List<Map<String, dynamic>>? gameScores,
     int? position,
     LeaderBoardStatus? leaderBoardStatus,
+    String? time,
   }) {
     return LeaderBoardState(
       gameScores: gameScores ?? this.gameScores,
       position: position ?? this.position,
       leaderBoardStatus: leaderBoardStatus ?? this.leaderBoardStatus,
+      time: time ?? this.time,
     );
   }
 
   @override
-  List<Object?> get props => [gameScores, position, leaderBoardStatus];
+  List<Object?> get props => [gameScores, position, leaderBoardStatus, time];
 }
