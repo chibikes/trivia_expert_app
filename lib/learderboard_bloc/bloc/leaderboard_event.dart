12d710fa00459e876c66e2b82ad73c2c64369b19
chibikes <okoh_chibuike@yahoo.com>
2023-07-03 20:33:55 +0100

    Add new event `LeaderBoardTime`

--- lib/learderboard_bloc/bloc/leaderboard_event.dart
+++ lib/learderboard_bloc/bloc/leaderboard_event.dart
@@ -2,19 +2,22 @@ import 'package:equatable/equatable.dart';
 
 abstract class LeaderBoardEvent extends Equatable {
   @override
-
   List<Object?> get props => [];
-
 }
 
 class CreateLeaderBoard extends LeaderBoardEvent {
   final List<Map<String, dynamic>> gameScores;
 
   CreateLeaderBoard(this.gameScores);
-
 }
 
-class GetUserId extends LeaderBoardEvent{
+class LeaderBoardTime extends LeaderBoardEvent {
+  final String time;
+
+  LeaderBoardTime(this.time);
+}
+
+class GetUserId extends LeaderBoardEvent {
   final String id;
 
   GetUserId(this.id);
