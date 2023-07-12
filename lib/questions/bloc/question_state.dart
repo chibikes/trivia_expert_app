5febbae1090d75431fe9f0a689fe191ab335f9cf
chibikes <okoh_chibuike@yahoo.com>
2023-07-12 16:20:51 +0100

    Add `QuestionType` as a `QuestionState` field

--- lib/questions/bloc/question_state.dart
+++ lib/questions/bloc/question_state.dart
@@ -2,6 +2,8 @@ part of 'question_bloc.dart';
 
 enum QuestionStatus { inProgress, success, failure }
 
+enum QuestionType { text, image }
+
 class QuestionState extends Equatable {
   final QuestionStatus status;
   final List<Questions> questions;
@@ -9,23 +11,27 @@ class QuestionState extends Equatable {
   final int offset;
   final int limit;
   final String category;
+  final QuestionType type;
 
   const QuestionState({
     this.limit = databaseLimit,
     this.offset = 0,
+    this.type = QuestionType.text,
     this.status = QuestionStatus.inProgress,
     this.questions = const <Questions>[],
     this.hasReachedMax = false,
     this.category = 'General',
   });
 
-  QuestionState copyWith(
-      {QuestionStatus? status,
-      List<Questions>? questions,
-      bool? hasReachedMax,
-      int? offset,
-      int? limit,
-      String? category}) {
+  QuestionState copyWith({
+    QuestionStatus? status,
+    List<Questions>? questions,
+    bool? hasReachedMax,
+    int? offset,
+    int? limit,
+    String? category,
+    QuestionType? type,
+  }) {
     return QuestionState(
       status: status ?? this.status,
       questions: questions ?? this.questions,
@@ -33,10 +39,11 @@ class QuestionState extends Equatable {
       offset: offset ?? this.offset,
       limit: limit ?? this.limit,
       category: category ?? this.category,
+      type: type ?? this.type,
     );
   }
 
   @override
   List<Object> get props =>
-      [status, questions, hasReachedMax, offset, limit, category];
+      [status, questions, hasReachedMax, offset, limit, category, type];
 }
